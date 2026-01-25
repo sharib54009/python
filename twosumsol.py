33ccc1b1d6785010f693d59d4f7cafa0fe274b06
sharib54009 <mohammedsharib0704@gmail.com>
2026-01-25 22:27:34 +0530

    codes practice

--- twosumsol.py
+++ twosumsol.py
@@ -10,3 +10,10 @@ def twoSum(nums, target):
 
         seen[num] = i
         print(f"Seen dict updated: {seen}")
+
+nums = [1, 5, 3, 8, 15]
+target = 9
+
+result = twoSum(nums, target)
+print(result)
+
